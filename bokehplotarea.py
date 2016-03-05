88807e66d733f4b53a5552b9c918c4670b517da4
Priyanka Vijay <pv225@cornell.edu>
2016-03-05 14:44:22 -0500

    text input

--- bokehplotarea.py
+++ bokehplotarea.py
@@ -16,9 +16,33 @@ data = dict(
     drubC=[22, 43, 10, 25, 26, 101, 114, 203, 194, 215, 201, 227, 139, 160],
 )
 
-tools = [PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool(),
-         HoverTool(), PreviewSaveTool()]
-p = Figure(plot_height=500, plot_width=500,
-           tools=tools, x_range=[0, 24])  # x_range=[0, 4*np.pi], y_range=[-2.5, 2.5]
-p.grid.grid_line_alpha = 0
+# set up figure
+# tools = [PanTool(), BoxZoomTool(), WheelZoomTool(), ResetTool(),
+#          HoverTool(), PreviewSaveTool()]
+# p = Figure(plot_height=500, plot_width=500,
+#            tools=tools, x_range=[0, 24])  # x_range=[0, 4*np.pi], y_range=[-2.5, 2.5]
+# p.grid.grid_line_alpha = 0
+
+# widgets
+text1 = TextInput(title="drug 1")
+text2 = TextInput(title="dose of drug 1")
+text3 = TextInput(title="drug 2")
+text4 = TextInput(title="dose of drug 2")
+
+
+# def update_data(attrname, old, new):
+#     t1 = text1.value
+#     t2 = text2.value
+#     t3 = text3.value
+#     t4 = text4.value
+#
+#
+# # callbacks
+# for t in [text1, text2, text3, text4]:
+#     w.on_change('value', update_data)
+
+# area plot
 area = Area(data, legend='top_left', title="Plasma Concentration over Time", xlabel="Time", ylabel="Concentration")
+
+inputs = VBoxForm(children=[text1, text2, text3, text4])
+curdoc().add_root(HBox(children=[inputs, area]))
